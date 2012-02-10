6b0c0bd7e0c3998c2bede3cb1ce5488370866ec8
Saida Trakhova <saidatrahova@mail.ru>
2012-02-10 17:47:01 +0400

    2nd commit

---
+++ views.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from django.http import Http404, HttpResponse
+import datetime
+
+def hello(request):
+    return HttpResponse("Здравствуй, Мир")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
